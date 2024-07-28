53f52877c7bbe6a74b6f52bd9e05012b1f78bd08
Djoule <61910104+stepasopruk@users.noreply.github.com>
2024-07-28 12:44:15 +0300

    objects grabbable

--- Assets/Scripts/Core/Interactible/MaterialChangerGrabbable.cs
+++ Assets/Scripts/Core/Interactible/MaterialChangerGrabbable.cs
@@ -1,41 +1,70 @@
 using BNG;
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
 public sealed class MaterialChangerGrabbable : GrabbableEvents
 {
-    [SerializeField] private Material standartMaterial;
-    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private MaterialObjectData materialData;
 
     private MeshRenderer _meshRenderer;
 
+    private bool _isSelect;
+    private bool IsSelect
+    {
+        get => _isSelect;
+        set
+        {
+            if (IsSelect == value)
+                return;
+
+            _isSelect = value;
+            ChangeMaterial();
+        }
+    }
+
+    private bool _isHover;
+    private bool IsHover
+    {
+        get => _isHover;
+        set
+        {
+            if (IsHover == value || IsSelect)
+                return;
+
+            _isHover = value;
+            ChangeMaterial();
+        }
+    }
+
     private void Start()
     {
-        _meshRenderer ??= GetComponent<MeshRenderer>();
-        _meshRenderer.sharedMaterial = standartMaterial;
-
-        SubscriptionEvents();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.materials = materialData.StandartMaterials;
     }
 
-    public override void OnGrab(Grabber grabber)
+    public override void OnGrab(Grabber grabber) 
+        => IsSelect = true;
+
+    public override void OnBecomesClosestGrabbable(ControllerHand touchingHand) 
+        => IsHover = true;
+
+    public override void OnNoLongerClosestGrabbable(ControllerHand touchingHand) 
+        => IsHover = false;
+
+    public override void OnBecomesClosestRemoteGrabbable(ControllerHand touchingHand) 
+        => IsHover = true;
+
+    public override void OnNoLongerClosestRemoteGrabbable(ControllerHand touchingHand) 
+        => IsHover = false;
+
+    public override void OnRelease() 
+        => IsSelect = false;
+
+    private void ChangeMaterial()
     {
-        if(highlightMaterial != null)
-            _meshRenderer.sharedMaterial = highlightMaterial;
-    }
-
-    public override void OnRelease()
-    {
-        _meshRenderer.sharedMaterial = standartMaterial;
-    }
-
-    private void SubscriptionEvents()
-    {
-
-    }
-
-    public void ChangeMaterial(Material material)
-    {
-
+        _meshRenderer.materials =
+            IsSelect ? materialData.SelectedMaterials :
+            IsHover ? materialData.HoverMaterials :
+            materialData.StandartMaterials;
     }
 }
